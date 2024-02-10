1fe0bffed746bfb5c3cdfec9272c5a906159d4ee
DrDreo <code@anderas-hahn.at>
2024-02-10 15:47:54 +0100

    try react

--- apps/shell/src/app/app.routes.ts
+++ apps/shell/src/app/app.routes.ts
@@ -1,19 +1,24 @@
-import { NxWelcomeComponent } from './nx-welcome.component';
-import { Route } from '@angular/router';
+import {NxWelcomeComponent} from './nx-welcome.component';
+import {Route} from '@angular/router';
 
 export const appRoutes: Route[] = [
-  {
-    path: 'creativeset-list',
-    loadChildren: () =>
-      import('creativeset-list/Routes').then((m) => m.remoteRoutes),
-  },
-  {
-    path: 'brand-settings',
-    loadChildren: () =>
-      import('brand-settings/Routes').then((m) => m.remoteRoutes),
-  },
-  {
-    path: '',
-    component: NxWelcomeComponent,
-  },
+    {
+        path: 'creativeset-list',
+        loadChildren: () =>
+            import('creativeset-list/Routes').then((m) => m.remoteRoutes),
+    },
+    {
+        path: 'brand-settings',
+        loadChildren: () =>
+            import('brand-settings/Routes').then((m) => m.remoteRoutes),
+    },
+    {
+        path: 'react-app',
+        loadChildren: () =>
+            import('react-app/Module').then((m) => m.remoteRoutes),
+    },
+    {
+        path: '',
+        component: NxWelcomeComponent,
+    },
 ];
